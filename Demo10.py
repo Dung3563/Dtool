63376ef7702185c789ea90ba211f03b8fd8f6b87
Dung3563 <91686770+Dung3563@users.noreply.github.com>
2021-12-11 20:19:06 +0700

    Add files via upload

--- Demo10.py
+++ Demo10.py
@@ -211,7 +211,8 @@ if ls==1:
                dem=dem+1
                t=datetime.now().strftime("%H:%M:%S")
                check_tk=requests.get(url='https://traodoisub.com/scr/user.php', headers=head
-               write(f"\033[1;93m[\033[1;93m{dem}\033[1;93m]\033[1;91|\033[1;96m{t}\033[1;91m • \033[1;92mLIKE\033[1;91m •\033[1;96m {id_like} \033[1;91m •\033[1;92m+300\033[1;91m • \033[1;93m{int(check_tk.json()['xu'])}\033[1;91m •")
+               print(dung) 
+               print(f"\033[1;93m[\033[1;93m{dem}\033[1;93m]\033[1;91|\033[1;96m{t}\033[1;91m • \033[1;92mLIKE\033[1;91m •\033[1;96m {id_like} \033[1;91m •\033[1;92m+300\033[1;91m • \033[1;93m{int(check_tk.json()['xu'])}\033[1;91m •")
                if dem==stop*nvdl:
                                     stop+=1
                                     for demtg in range(dlnv, -1,-1):
